2dc36016da0352521fa5c7fa9ddf4408f9be52fc
nurulamin092 <mnurulamin092@gmial.com>
2024-02-14 06:48:37 +0600

    custom hook

--- src/page/Page.jsx
+++ src/page/Page.jsx
@@ -4,13 +4,13 @@ import WeatherBoard from "../components/Weather/WeatherBoard";
 
 export default function Page() {
   return (
-    <>
+    <div className="grid place-items-center h-screen">
       <Header />
       <main>
         <section>
           <WeatherBoard />
         </section>
       </main>
-    </>
+    </div>
   );
 }
